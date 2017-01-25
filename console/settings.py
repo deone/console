399fd71185656540d99ea6869694637c16d3557b
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-01-25 18:18:15 +0000

    Create database.

--- console/settings.py
+++ console/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'console.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        },
+        'NAME': 'console',
+        'USER': 'console_admin',
+        'PASSWORD': 'c0ns0l3',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
---
+++ db.sql
@@ -0,0 +1,2 @@
+create database console;
+grant all on console.* to 'console_admin'@'localhost' identified by 'c0ns0l3';
