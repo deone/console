c23edf4a2582dc02983d666e5899b8e7c7a55738
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-01-27 11:29:03 +0000

    - Base template. - Static files.

--- console/settings.py
+++ console/settings.py
@@ -126,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# My Settings
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
